be61aca31ab61c744a972ddaed2c021b1584bf25
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-10-11 11:08:51 +0200

    upd
    upd

---
+++ src/partials/mark-up.js
@@ -0,0 +1,44 @@
+export { generateMainBookMarkup, generateShoppingListMarkup, generateModalWindowMarkup };
+
+
+
+
+
+function generateMainBookMarkup(books) {
+  return books.map(book => `
+    <div class="image-container">
+      <img class="book-image" src="${book.book_image}" alt="${book.name}">
+      <div class="info-book">
+        <h1 class="title">${book.name}</h1>
+        <p class="author">${book.author}</p>
+      </div>
+    </div>
+  `).join('');
+}
+
+function generateShoppingListMarkup(books) {
+  return books.map(book => `
+    <div class="image-container">
+      <img class="book-image" src="${book.book_image}" alt="${book.name}">
+      <div class="info-book">
+        <h1 class="title">${book.name}</h1>
+        <p class="list-name">${book.list_name}</p>
+        <p class="description">${book.description}</p>
+        <p class="author">${book.author}</p>
+      </div>
+    </div>
+  `).join('');
+}
+
+function generateModalWindowMarkup(books) {
+  return books.map(book => `
+    <div class="image-container">
+      <img class="book-image" src="${book.book_image}" alt="${book.name}">
+      <div class="info-book">
+        <h1 class="title">${book.name}</h1>
+        <p class="author">${book.author}</p>
+        <p class="description">${book.description}</p>
+      </div>
+    </div>
+  `).join('');
+}
