802de416aa1ce2ad6cbdbc84d37c340e05085488
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-10-12 01:21:08 +0200

    upd
    upd

--- src/js/mark-up.js
+++ src/js/mark-up.js
@@ -1,52 +1,87 @@
-export { generateMainBookMarkup, generateShoppingListMarkup, generateModalWindowMarkup };
+export { createTopMarkup, createCategoryMarkup, createModalWindowMarkup, createShoppingListMarkup };
+
+
+
+  
+    function createTopMarkup(book) {
+     return ` <h2 class="second-title">${book.list_name}</h2>
+     <div class="image-container">
+       <img class="book-image" src="${book.books[0].book_image}" alt="${book.books[0].title}">
+       <div class="info-book">
+         <h1 class="title">${book.books[0].title}</h1>
+         <p class="author">${book.books[0].author}</p>
+       </div>
+       <div class="image-container">
+       <img class="book-image" src="${book.books[1].book_image}" alt="${book.books[0].title}">
+       <div class="info-book">
+         <h1 class="title">${book.books[1].title}</h1>
+         <p class="author">${book.books[1].author}</p>
+       </div>
+       <div class="image-container">
+       <img class="book-image" src="${book.books[2].book_image}" alt="${book.books[0].title}">
+       <div class="info-book">
+         <h1 class="title">${book.books[2].title}</h1>
+         <p class="author">${book.books[2].author}</p>
+       </div>
+       <div class="image-container">
+       <img class="book-image" src="${book.books[3].book_image}" alt="${book.books[0].title}">
+       <div class="info-book">
+         <h1 class="title">${book.books[3].title}</h1>
+         <p class="author">${book.books[3].author}</p>
+       </div>
+       <div class="image-container">
+       <img class="book-image" src="${book.books[4].book_image}" alt="${book.books[4].title}">
+       <div class="info-book">
+         <h1 class="title">${book.books[4].title}</h1>
+         <p class="author">${book.books[4].author}</p>
+       </div>
+       <button type="button" class="see-more">See more</button>
+     </div>`
+   }
 
 
 
 
 
-function generateMainBookMarkup(books) {
-  return books.map(book => `
-    <div class="image-container">
-      <img class="book-image" src="${book.book_image}" alt="${book.name}">
-      <div class="info-book">
-        <h1 class="title">${book.name}</h1>
-        <p class="author">${book.author}</p>
-      </div>
-    </div>
-  `).join('');
-    
-     const mainMarkUp = generateMainBookMarkup(books);
+function createCategoryMarkup(book) {
+  return `
+     <div class="image-container">
+       <img class="book-image" src="${book.books[0].book_image}" alt="${book.books[0].title}">
+       <div class="info-book">
+         <h1 class="title">${book.books[0].title}</h1>
+         <p class="author">${book.books[0].author}</p>
+       </div>
+  `
+     
 }
 
-function generateShoppingListMarkup(books) {
-  return books.map(book => `
+
+
+function createShoppingListMarkup(book) {
+  return `
     <div class="image-container">
-      <img class="book-image" src="${book.book_image}" alt="${book.name}">
+      <img class="book-image" src="${book.books[0].book_image}" alt="${book.books[0].title}">
       <div class="info-book">
-        <h1 class="title">${book.name}</h1>
+        <h1 class="title">${book.books[0].title}</h1>
         <p class="list-name">${book.list_name}</p>
-        <p class="description">${book.description}</p>
-        <p class="author">${book.author}</p>
+        <p class="description">${book.books[0].description}</p>
+        <p class="author">${book.books[0].author}</p>
       </div>
     </div>
-  `).join('');
+  `
     
-    const shoppingListMarkUp = generateShoppingListMarkup(books);
 }
 
-function generateModalWindowMarkup(books) {
-  return books.map(book => `
+function createModalWindowMarkup(book) {
+  return `
     <div class="image-container">
-      <img class="book-image" src="${book.book_image}" alt="${book.name}">
+      <img class="book-image" src="${book.books[0].book_image}" alt="${book.books[0].title}">
       <div class="info-book">
-        <h1 class="title">${book.name}</h1>
-        <p class="author">${book.author}</p>
-        <p class="description">${book.description}</p>
+        <h1 class="title">${book.books[0].title}</h1>
+        <p class="author">${book.books[0].author}</p>
+        <p class="description">${book.books[0].description}</p>
       </div>
     </div>
-  `).join('');
-    
-    const modalWindowMarkup = generateModalWindowMarkup(books);
-
+  `
     
 }
