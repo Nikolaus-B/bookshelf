07c8956d17e75586d769f96a9ebc888c137e9b40
SvitLana1990 <svitlana112@outlook.es>
2023-10-13 07:44:42 +0200

    project-header
    without checkbox

---
+++ src/js/header.js
@@ -0,0 +1,28 @@
+export { handlerClickMenu };
+
+import refs from './refs';
+
+const {
+  headerLabel,
+  headerInput,
+  headerButton,
+  headerHamburgerIcon,
+  headerCloseIcon,
+  headerUserIcon,
+  openMenuBtn,
+  mobileMenu,
+} = refs;
+
+openMenuBtn.addEventListener('click', handlerClickMenu);
+
+function handlerClickMenu() {
+  mobileMenu.classList.toggle('is-open');
+
+  if (mobileMenu.classList.contains('is-open')) {
+    headerHamburgerIcon.style.display = 'none';
+    headerCloseIcon.style.display = 'block';
+  } else {
+    headerHamburgerIcon.style.display = 'block';
+    headerCloseIcon.style.display = 'none';
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,1 @@
-
+import { handlerClickMenu } from './js/header';
