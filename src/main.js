ffae0d66614827f0bf9a4819582262eaec074205
SvitLana1990 <svitlana112@outlook.es>
2023-10-14 12:14:47 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ document.addEventListener(
   'DOMContentLoaded',
   logoModuleFunction.onDOMContentLoaded
 );
-
 import { renderCategories } from './js/categories';
 import createMarkup from './js/top-books';
+// import { scrollFunction } from './js/scroll-up';
