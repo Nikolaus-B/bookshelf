9261054d01e6a9897564a92764f2c58dff444017
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2023-10-17 01:24:00 +0300

    Update mark-up.js

--- src/js/mark-up.js
+++ src/js/mark-up.js
@@ -42,7 +42,6 @@ function createTopMarkup(book) {
 }
 
 function createCategoryMarkup(book) {
-  console.log(book);
   return `
   <div class="image-container">
        <img class="book-image" src="${book.book_image}" alt="${book.title}">
